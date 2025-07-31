9ea9fa0073fb21eddef9fba014d40c9bedb91e74
wr <wr@qq.com>
2025-07-31 11:54:11 +0800

    工人->出工记录: controller中方法添加日志; 工人->出工记录: 查询时将人员名称和手机号赋值到实体类中; 工人->工人: 分页查询最后出工月份排序问题修复;

--- src/main/java/com/dc/ncsys_springboot/controller/WorkerAttendanceController.java
+++ src/main/java/com/dc/ncsys_springboot/controller/WorkerAttendanceController.java
@@ -27,37 +27,66 @@ public class WorkerAttendanceController {
     @Autowired
     private WorkerAttendanceService workerAttendanceService;
 
+    /**
+     * 获取指定月份的工人出工记录
+     * @param ym 年月
+     * @return 工人出工记录列表
+     */
     @GetMapping("/getWorkerAndAttendanceList")
     public ResVo<List<Map<String, Object>>> getWorkerAndAttendanceList(String ym) {
         log.info("CONT入参: {}", ym);
         return workerAttendanceService.getWorkerAndAttendanceList(ym);
     }
 
+    /**
+     * 添加工人出工记录
+     * @param workerAttendanceDo 工人出工记录
+     * @return 操作结果
+     */
     @PostMapping("/addWorkerAttendance")
     public ResVo<Object> addWorkerAttendance(@RequestBody WorkerAttendanceDo workerAttendanceDo) {
         log.info("CONT入参: {}", workerAttendanceDo);
         return workerAttendanceService.addWorkerAttendance(workerAttendanceDo);
     }
 
+    /**
+     * 更新工人出工记录
+     * @param workerAttendanceDo 工人出工记录
+     * @return 操作结果
+     */
     @PostMapping("/updateWorkerAttendance")
     public ResVo<Object> updateWorkerAttendance(@RequestBody WorkerAttendanceDo workerAttendanceDo) {
         log.info("CONT入参: {}", workerAttendanceDo);
         return workerAttendanceService.updateWorkerAttendance(workerAttendanceDo);
     }
 
+    /**
+     * 获取工人所有出工记录
+     * @param workerDo 工人
+     * @return 工人出工记录列表
+     */
     @PostMapping("/getWorkerAllAttendance")
     public ResVo<List<WorkerAttendanceDo>> getWorkerAllAttendance(@RequestBody WorkerDo workerDo) {
         log.info("CONT入参: {}", workerDo);
         return workerAttendanceService.getWorkerAllAttendance(workerDo);
     }
 
+    /**
+     * 获取工人所有出工记录，按年月分组
+     * @param workerDo 工人
+     * @return 工人出工记录列表
+     */
     @PostMapping("/getWorkerAllAttendanceGroupByYm")
     public ResVo<List<Map<String, Object>>> getWorkerAllAttendanceGroupByYm(@RequestBody WorkerDo workerDo) {
         log.info("CONT入参: {}", workerDo);
         return workerAttendanceService.getWorkerAllAttendanceGroupByYm(workerDo);
     }
 
-
+    /**
+     * 获取工人所有出工记录，按年月分组
+     * @param workerDo 工人
+     * @return 工人出工记录Map
+     */
     @PostMapping("/getWorkerAllAttendanceGroupByYm2")
     public ResVo<Map<String, Object>> getWorkerAllAttendanceGroupByYm2(@RequestBody WorkerDo workerDo) {
         log.info("CONT入参: {}", workerDo);
